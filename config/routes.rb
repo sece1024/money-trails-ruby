720713c400cd181e774a564dd9b01ccb5ba73df7
gaoxiang <sece1024@gmail.com>
2025-09-17 16:01:35 +0800

    feat: add password update functionality with form and controller

--- config/routes.rb
+++ config/routes.rb
@@ -20,4 +20,8 @@ Rails.application.routes.draw do
     resources :subscribers, only: [ :create ]
   end
   resource :unsubscribe, only: [ :show ]
+
+  namespace :settings do
+    resource :password, only: [ :show, :update ]
+  end
 end
